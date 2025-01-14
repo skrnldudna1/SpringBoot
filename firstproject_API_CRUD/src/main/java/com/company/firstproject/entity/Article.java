7f837e12982ae76d46a27f244dde52706aa05e3f
skrnldudna1 <skrnldudna1@naver.com>
2025-01-14 13:08:42 +0900

    API_CRUD

---
+++ firstproject_API_CRUD/src/main/java/com/company/firstproject/entity/Article.java
@@ -0,0 +1,27 @@
+package com.company.firstproject.entity;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.cglib.core.ClassGenerator;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String title;
+
+
+    private String content;
+
+    public void patch(Article article) {
+    }
+}
