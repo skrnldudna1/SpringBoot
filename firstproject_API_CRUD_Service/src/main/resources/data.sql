7b27df95025dc28fe2b8bde682fbcfb4232dc125
skrnldudna1 <skrnldudna1@naver.com>
2025-01-17 12:01:12 +0900

    22

--- firstproject_API_CRUD_Service/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO article(id, title, content) VALUES(1, '히히히히', '11111');
-INSERT INTO article(id, title, content) VALUES(2, '하하하하', '22222');
-INSERT INTO article(id, title, content) VALUES(3, '호호호호', '33333');
-
-SELECT * FROM article;
