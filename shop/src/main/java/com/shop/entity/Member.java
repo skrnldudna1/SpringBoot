c53401afe894b2984df8b9f2b9b179771cdfc7af
skrnldudna1 <skrnldudna1@naver.com>
2025-01-23 14:12:42 +0900

    SpringBoot쇼핑몰제작/시큐리티까지

---
+++ shop/src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,49 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFormDto;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto,
+                                      PasswordEncoder passwordEncoder){
+        Member member = Member.builder()
+                .role(Role.User)
+                .email(memberFormDto.getEmail())
+                .address(memberFormDto.getAddress())
+                .name(memberFormDto.getName())
+                .password(passwordEncoder.encode(memberFormDto.getPassword()))
+                .build();
+
+        String password = memberFormDto.getPassword();
+
+        return member;
+    }
+
+}
---
+++ shop/src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package com.shop.repository;
+
+import com.querydsl.core.BooleanBuilder;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>,
+        QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value = "SELECT * FROM Item i WHERE i.item_detail LIKE %:itemDetail% ORDER BY i.price DESC", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+
+}
